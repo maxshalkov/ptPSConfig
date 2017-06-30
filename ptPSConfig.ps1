4ff750b1d42dcbb1b0fdb70a3a75f1f4c88077c5
maxshalkov <max.shalkov@gmail.com>
2017-06-30 15:47:21 +0300

    Start

---
+++ ptPSConfig.ps1
@@ -0,0 +1,6 @@
+﻿$Domain = $env:USERDNSDOMAIN -split "\." # or other domain
+
+$Searcher = [ADSISearcher]"(&(objectclass=group)(cn=psconfig))"  
+$Searcher.SearchRoot = "LDAP://DC=$($Domain[0]),DC=$($Domain[1])"
+$Searcher.PropertiesToLoad.Add("info") | Out-Null
+$psconfig_path = ($Searcher.FindAll().properties.info -split "\n")[0].trim()
